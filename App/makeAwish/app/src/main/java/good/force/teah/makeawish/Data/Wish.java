7038aabea392660cd5a263e1e92fad001574f1ee
Miloy03 <miloyajmera.12@gmail.com>
2023-02-18 20:46:59 -0500

    Update Wish.java

--- App/makeAwish/app/src/main/java/good/force/teah/makeawish/Data/Wish.java
+++ App/makeAwish/app/src/main/java/good/force/teah/makeawish/Data/Wish.java
@@ -4,7 +4,46 @@ package good.force.teah.makeawish.Data;
  * Created by Anurag on 7/30/2017.
  */
 
-public class Wish {
-    private String type;
+public class wish {
+    private String name,status;
+    private int imageResId;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public int getImageResId() {
+        return imageResId;
+    }
+
+    public void setImageResId(int imageResId) {
+        this.imageResId = imageResId;
+    }
+
+    public wish() {
+    }
+
+    public  wish (String name, String status){
+        this.name = name;
+        this.status = status;
+    }
+    public wish(String name, String status, int imageResId) {
+
+        this.name = name;
+        this.status = status;
+        this.imageResId = imageResId;
+    }
     //private
 }
